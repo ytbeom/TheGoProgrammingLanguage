2fb6749243c184cf1f5263f54fc28c0a1e91b60b
ytbeom <ytbeom@gmail.com>
2021-03-24 13:03:15 +0900

    Add ch2 example codes

---
+++ ch2/tempconv0.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+
+func main() {
+	c := FToC(212.0)
+	fmt.Println(c.String()) // "100°C"
+	fmt.Printf("%v\n", c)   // "100°C"; no need to call String explicitly
+	fmt.Printf("%s\n", c)   // "100°C"
+	fmt.Println(c)          // "100°C"
+	fmt.Printf("%g\n", c)   // "100"; does not call String
+	fmt.Println(float64(c)) // "100"; does not call String
+
+	// Output:
+	// 100°C
+	// 100°C
+	// 100°C
+	// 100°C
+	// 100
+	// 100
+}
